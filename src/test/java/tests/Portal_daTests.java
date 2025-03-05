1d54b9927a92fa2541eaac4433f94cba484c16d3
Alisa Perepelova <alice-lilo@mail.ru>
2025-03-05 22:52:24 +0300

    Initial commit

--- src/test/java/tests/Portal_daTests.java
+++ src/test/java/tests/Portal_daTests.java
@@ -1,22 +1,19 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.CookiePopUp;
 
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
 import static com.codeborne.selenide.CollectionCondition.texts;
-import static com.codeborne.selenide.Condition.interactable;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static io.qameta.allure.Allure.step;
 
 public class Portal_daTests extends TestBase {
-  CookiePopUp cookie= new CookiePopUp();
+  CookiePopUp cookie = new CookiePopUp();
 
 
   @Test
@@ -34,9 +31,10 @@ public class Portal_daTests extends TestBase {
     Selenide.clearBrowserCookies();
     Selenide.closeWebDriver();
   }
+
   @Test
   @DisplayName("Проверка поиска {} по строке ввода")
-  void searchByInputString(){
+  void searchByInputString() {
     open("/");
     cookie.checkCookiePopupDisplay();
     cookie.acceptCookie();
@@ -47,43 +45,44 @@ public class Portal_daTests extends TestBase {
     Selenide.clearBrowserCookies();
     Selenide.closeWebDriver();
   }
+
   @Test
   @DisplayName("Проверка контроллов")
-  void checkControl(){
+  void checkControl() {
     Configuration.pollingInterval = 500;
     open("/");
     cookie.checkCookiePopupDisplay();
     cookie.acceptCookie();
     sleep(1500);
     $(".search-bar-item__right").click();
-    $(byText("Офисное помещение")).click();
-    $("div.listing-footer-controls__more span").shouldBe(interactable);
-
-    $("div.listing-footer-controls__more span").scrollIntoView(true);
-
-    $("div.listing-footer-controls__more span").click();
-
-    int i = 1;
-
-    $(".listing-footer-controls__count").shouldHave(text("Показано 18"));
+//    $(byText("Офисное помещение")).click();
+    sleep(1500);
+    $(".v-select__slot").scrollIntoView(true);
+    ;
+    $(".v-select__slot").click();
+    $(byText("54")).click();
+    sleep(1500);
+    $(".listing-footer-controls__count").shouldHave(text("Показано 54"));
 
     closeWebDriver();
-
-
-//    sleep(2000);
-//    executeJavaScript("window.scrollTo(0, document.body.scrollHeight);");
-////    $(".v-icon__svg").click();
-////    $(byText("54")).click();
-//    executeJavaScript("window.scrollTo(0, document.body.scrollHeight);");
-//    sleep(5000);
-//    $("div.listing-footer-controls__more span").click();
-//    sleep(5000);
-//    //$(".listing-footer-controls__more button").click();
-//    int childCount = $$(".catalog-grid > 54").size();
-//   // $$(".catalog-grid").shouldHave(sizeGreaterThanOrEqual(54));
-//    Selenide.clearBrowserCookies();
-//    Selenide.closeWebDriver();
+  }
+  @Test
+  @DisplayName("Проверка поиска {} в локации {}")
+  void checkSearchWithLocation() {
+    open("/");
+    cookie.checkCookiePopupDisplay();
+    cookie.acceptCookie();
+  //  $(".mr-2.icon.icon-catalog-list.icon-24").click();
+    $(byText("Фильтры")).click();
+    $(".base-dialog-card--nopadding").shouldBe(visible);
+    $(byText("Все регионы и населенные пункты")).shouldBe(visible).click();
+    $(byText("Москва")).click();
+    $(byText("Готово")).click();
+    $("#app").$(".filter-box.ui-clickable").sibling(2).click();
+    $("#filter-sale_type-direct_purchase").click();
+    $("#filter-sale_type-trade").click();
+    $("#filter-sale_type-bidding").click();
+    $(byText("Готово")).click();
 
   }
-//  executeJavaScript("window.scrollTo(0, document.body.scrollHeight);");
 }
