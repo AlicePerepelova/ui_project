b528b52fefa9e865be23fa4aa4d98022a1f6a70f
Alisa Perepelova <alice-lilo@mail.ru>
2025-03-14 20:42:28 +0300

    Initial commit

--- src/test/java/config/WebDriverProvider.java
+++ src/test/java/config/WebDriverProvider.java
@@ -1,19 +1,22 @@
 package config;
 
 import com.codeborne.selenide.Configuration;
+import lombok.extern.slf4j.Slf4j;
+import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 
 import java.util.Map;
 
+@Slf4j
 public class WebDriverProvider {
-  private final WebConfig config;
+  private static WebConfig config = ConfigFactory.create(WebConfig.class, System.getProperties());;
 
   public WebDriverProvider(WebConfig config){
     this.config = config;
   }
 
-  public void setUp() {
+  public static void setUp() {
     Configuration.baseUrl = config.getBaseUrl();
     Configuration.browser = config.getBrowser().toString();
     Configuration.browserSize = config.getBrowserSize();
