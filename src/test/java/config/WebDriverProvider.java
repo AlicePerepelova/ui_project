87f3a8388de43d064e1a8bc022031ed9cc29632c
Alisa Perepelova <alice-lilo@mail.ru>
2025-03-16 13:04:29 +0300

    delete config classes

--- src/test/java/config/WebDriverProvider.java
+++ src/test/java/config/WebDriverProvider.java
@@ -1,16 +1,14 @@
 package config;
 
 import com.codeborne.selenide.Configuration;
-import lombok.extern.slf4j.Slf4j;
 import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 
 import java.util.Map;
 
-@Slf4j
 public class WebDriverProvider {
-  private static WebConfig config = ConfigFactory.create(WebConfig.class, System.getProperties());;
+  private static WebConfig config = ConfigFactory.create(WebConfig.class, System.getProperties());
 
   public WebDriverProvider(WebConfig config){
     this.config = config;
